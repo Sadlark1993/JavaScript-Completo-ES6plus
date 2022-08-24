9a17c3f6357208aea69ec8408ae198d48d18b2f2
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-08-23 20:18:58 -0400

    Refatorei o modal.js

--- 10ProjetoFinalRefatorado/js/modules/modal.js
+++ 10ProjetoFinalRefatorado/js/modules/modal.js
@@ -1,23 +1,34 @@
 
-export default function initModal(){
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containerModal){
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+
+    //definir o objeto dessa classe como o this no metodo. Se nao fizer isso, o this sera o link q o chamou.
+    this.toggleModal = this.toggleModal.bind(this);
+    this.cliqueFora = this.cliqueFora.bind(this);
+  }
   
-  function toggleModal(event){
+  
+  toggleModal(event){
     event.preventDefault();
-    containerModal.classList.toggle('ativo');
+    this.containerModal.classList.toggle('ativo');
   }
 
-  function cliqueFora(event){
+  cliqueFora(event){
     if(event.target === event.currentTarget){
-      toggleModal(event);
+      this.toggleModal(event);
     }
   }
 
-  if(botaoAbrir&&botaoFechar&&containerModal){
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueFora);
+  init(){
+    if(this.botaoAbrir&&this.botaoFechar&&this.containerModal){
+      this.botaoAbrir.addEventListener('click', this.toggleModal);
+      this.botaoFechar.addEventListener('click', this.toggleModal);
+      this.containerModal.addEventListener('click', this.cliqueFora);
+    }
   }
+
+  
 }
--- 10ProjetoFinalRefatorado/js/script.js
+++ 10ProjetoFinalRefatorado/js/script.js
@@ -3,10 +3,11 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordeon from "./modules/accordeon.js";
 import TabNav from "./modules/tab-nav.js";
+import Modal from "./modules/modal.js";
 
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
-import initModal from "./modules/modal.js";
+
 import initTooltip from "./modules/tooltip.js"
 import initDropdown from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -24,9 +25,10 @@ accordeon.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
+modal.init();
 
 initAnimacaoScroll();
-initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
